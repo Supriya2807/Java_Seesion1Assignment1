ccc14803f2b58c31c027bf7d10d3c0730ed54bdf
Supriya2807 <lahadesupriya@gmail.com>
2016-05-26 06:59:01 +0530

    First Assignment
    First Assignment

---
+++ addition.java
@@ -0,0 +1,17 @@
+package acadgild.assignments;
+
+public class addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=4,result;
+		float b= 3.76f;
+		result= a+ (int)b;
+		
+	    System.out.println("The addition of a and b gives: "+result);
+	    
+		
+	}
+
+}
